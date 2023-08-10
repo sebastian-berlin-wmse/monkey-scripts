fbcca6fd8406973f9e13b3ba20e08be5b7faafe4
sebastian-berlin-wmse <36327337+sebastian-berlin-wmse@users.noreply.github.com>
2023-08-10 16:45:32 +0200

    Create fortnox-flex.js

---
+++ fortnox-flex.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         Fortnox flex
+// @namespace    sebastian.berlin@wikimedia.se
+// @version      0.0.1
+// @description  Highlight flex time rows
+// @author       Sebastian Berlin
+// @match        https://apps5.fortnox.se/time-reporting/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=fortnox.se
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    let observer = new MutationObserver((records) => {
+        records.forEach((mutation) => {
+            if(mutation.addedNodes) {
+                for(let node of mutation.addedNodes) {
+                    if(node.textContent === "Flextid +") {
+                        let row = node.parentElement.closest("tr");
+                        row.style.background = "salmon";
+                    }
+                }
+            }
+        });
+    });
+
+    observer.observe(document.body, {
+        subtree: true,
+        childList: true
+    });
+})();
