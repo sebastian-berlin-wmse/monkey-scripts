57309f6f1fd427c7a98599680eb5c228cab5a6d9
sebastian-berlin-wmse <36327337+sebastian-berlin-wmse@users.noreply.github.com>
2024-03-20 10:20:46 +0100

    Hide main chat when viewing threads thread
    Adds a button in the divider between the chat panes that toggles visibility of the main thread. Also breaks out the preview toggling to a separate function.

--- google-chat-toggle-previews.js
+++ google-chat-toggle-previews.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Toggle previews
-// @namespace    http://tampermonkey.net/
-// @version      0.1.3
-// @description  Hide previews and allow toggling their visibility.
+// @namespace    sebastian.berlin@wikimedia.se
+// @version      0.1.4
+// @description  Hide previews and allow toggling their visibility. Hide main chat when viewing threads.
 // @author       Sebastian Berlin
 // @match        https://chat.google.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com
@@ -16,17 +16,74 @@
         console.log("[Toggle previews] ", ...args);
     }
 
+    function addMainToggle(mutation) {
+        if(mutation.type !== "attributes" || !mutation.target.matches(".XN4klc.eO2Zfd")) {
+            return;
+        }
+
+        let divider = mutation.target;
+        let dividerHandle = divider.querySelector(".eKEW9c");
+        if(dividerHandle.querySelector(".main-toggle")) {
+            // Only add the button once.
+            return;
+        }
+
+        let button = document.createElement("button");
+        dividerHandle.appendChild(button);
+        button.textContent = "◀";
+        button.classList.add("main-toggle");
+        button.addEventListener("click", e => {
+            let main = document.querySelector(".RWqrJc");
+            main.hidden = !main.hidden;
+        });
+    }
+
+    function addPreviewToggles(mutation) {
+        for(let node of mutation.addedNodes) {
+            if(node instanceof Element) {
+                let previews = node.querySelectorAll(".QPe6Ge");
+                for(let preview of previews) {
+                    let content = preview.firstChild;
+                    if(!content.hasChildNodes()) {
+                        continue;
+                    }
+
+                    if(preview.parentNode.querySelector(".preview-toggle")) {
+                        // Toggle has already been added here.
+                        continue;
+                    }
+
+                    let button = document.createElement("button");
+                    button.textContent = "▼";
+                    button.classList.add("preview-toggle");
+                    if(preview.matches(".nF6pT.yqoUIf.Fxfmxb .QPe6Ge")) {
+                        button.classList.add("your");
+                    } else {
+                        button.classList.add("others");
+                    }
+                    button.addEventListener("click", () => {
+                        preview.hidden = !preview.hidden
+                    });
+                    preview.before(button);
+                    log("Added button", button, "for", preview);
+
+                    preview.hidden = true;
+                }
+            }
+        }
+    }
+
     let style = document.createElement("style")
     document.head.appendChild(style)
     let sheet = Array.from(document.styleSheets).at(-1);
     sheet.insertRule(
         `.preview-toggle {
-            background: transparent;
-            border: none;
-            width: 30px;
-            height: 30px;
-            border-radius: 30px;
-        }`
+             background: transparent;
+             border: none;
+             width: 30px;
+             height: 30px;
+             border-radius: 30px;
+         }`
     );
     sheet.insertRule(
         `.preview-toggle:hover {
@@ -35,74 +92,58 @@
     );
     sheet.insertRule(
         `.preview-toggle.your:has(~ .QPe6Ge[hidden]) {
-            rotate: 90deg;
-        }`
+             rotate: 90deg;
+         }`
     );
     sheet.insertRule(
         `.preview-toggle.others:has(~ .QPe6Ge[hidden]) {
-            rotate: -90deg;
-        }`
+             rotate: -90deg;
+         }`
     );
     sheet.insertRule(
         `.preview-toggle.your {
-            align-self: end;
-        }`
+             align-self: end;
+         }`
     );
     sheet.insertRule(
         `.preview-toggle.others {
-            align-self: start;
-        }`
+             align-self: start;
+         }`
+    );
+    sheet.insertRule(
+        `.main-toggle {
+             position: relative;
+             left: -7px;
+             top: 50%;
+             transform: translateY(-50%);
+             color: var(--secondary-text-color);
+             padding: 2px;
+             padding-left: 1px;
+             background: rgb(241,243,244);
+             border: solid 2px var(--gm3-color-outline);
+             border-radius: 50%;
+         }`
+    );
+    sheet.insertRule(
+        `.RWqrJc[hidden] ~ .XN4klc.eO2Zfd .main-toggle {
+             rotate: 180deg;
+             transform: translateY(50%);
+         }`
     );
 
     log("Starting...");
     let observer = new MutationObserver((records) => {
         records.forEach((mutation) => {
-            if(!mutation.addedNodes) {
-                return;
-            }
-
-            for(let node of mutation.addedNodes) {
-                if(node instanceof Element) {
-                    let previews = node.querySelectorAll(".QPe6Ge");
-                    if(previews.length == 0) {
-                        return;
-                    }
-
-                    for(let preview of previews) {
-                        let content = preview.firstChild;
-                        if(!content.hasChildNodes()) {
-                            continue;
-                        }
-
-                        if(preview.parentNode.querySelector(".preview-toggle")) {
-                            // Toggle has already been added here.
-                            continue;
-                        }
-
-                        let button = document.createElement("button");
-                        button.textContent = "▼";
-                        button.classList.add("preview-toggle");
-                        if(preview.matches(".nF6pT.yqoUIf.Fxfmxb .QPe6Ge")) {
-                            button.classList.add("your");
-                        } else {
-                            button.classList.add("others");
-                        }
-                        button.addEventListener("click", () => {
-                            preview.hidden = !preview.hidden
-                        });
-                        preview.before(button);
-                        log("Added button", button, "for", preview);
-
-                        preview.hidden = true;
-                    }
-                }
-            }
+            addPreviewToggles(mutation);
+            addMainToggle(mutation);
         });
     });
 
     observer.observe(document.body, {
         subtree: true,
-        childList: true
+        childList: true,
+        attributes: true
     });
 
 })();
+
